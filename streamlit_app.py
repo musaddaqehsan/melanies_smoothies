d9cad5808b3f93cf602f19540fedd5af6c2cae30
Musaddaq Ehsan <148864305+musaddaqehsan@users.noreply.github.com>
2026-04-15 12:52:00 +0200

    Update API call to use dynamic search value

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.header(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen)  
+        fruityvice_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on})  
         
         sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     #st.write(ingredients_string)
